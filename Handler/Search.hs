79de41da6860115710d3379ffce77e6f52b7685d
Matthew Spellings <mspells@umich.edu>
2013-08-18 09:49:41 -0400

    Made search more powerful

--- Handler/Search.hs
+++ Handler/Search.hs
@@ -2,6 +2,7 @@
 module Handler.Search where
 
 --import qualified Data.Conduit.List as C
+import Yesod.Markdown
 import Data.Maybe
 import Database.Persist.MongoDB
 import qualified Data.Map as M
@@ -9,24 +10,38 @@ import qualified Data.Text as T
 import Text.Parsec
 
 import Import
+import Barch.QueryParser as Q
 --import Barch.Adaptors
 
-type QueryKey = Text
-type QueryVal = Text
+-- query2DBFilter: take a list of query elements and return the
+-- database query to run
+query2DBFilter::[Elt]->[Filter Reference]
+query2DBFilter =
+  mapMaybe f
+  where
+    f (Q.Field t _) = Just $ multiEq ReferenceFieldKeys t
+    f (Tag t) = Just $ multiEq ReferenceTags t
+    f _ = Nothing
 
-text2Query::Text->[Text]
-text2Query = T.splitOn " "
+-- query2Filter: take a list of query elements and return the function
+-- to filter the results of the databse query by; assumes that tags
+-- have already been checked, for example
+query2Filter::[Elt]->(Entity Reference->Bool)
+query2Filter q =
+  \ref -> foldl (&&) True $ pure f <*> q <*> pure ref
+  where
+    f (Q.Field k v) (Entity _ ref) = T.isInfixOf v ((referenceFields ref) M.! k)
+    f (Plain t) (Entity _ ref) = t `occursIn` ref
+    f (Tag t) _ = True
 
--- convert the key component a query "path" into a db Filter
-queryKeys2Filter::[QueryKey]->[Filter Reference]
-queryKeys2Filter = map (multiEq ReferenceFieldKeys)
-
-querySplit::[Text]->[(QueryKey, QueryVal)]
-querySplit = map ((\(x, y)->(x, T.tail y)) . (T.breakOn ":"))
-
-queryFilterRef::[(QueryKey, QueryVal)]->Entity Reference->Bool
-queryFilterRef q (Entity _ (Reference _ _ fields _ _ _)) =
-  all (\(key, val)->T.isInfixOf val (fields M.! key)) q
+-- occursIn: return true if the given text occurs in any fields of a reference
+occursIn::Text->Reference->Bool
+occursIn txt (Reference typ id fields _ tags notes) =
+  inList [typ, id, unMarkdown notes] || inFields || inTags
+  where
+    inList = any (T.isInfixOf txt)
+    inFields = (inList . M.keys) fields || (inList . M.elems) fields
+    inTags = inList tags
 
 -- This is a handler function for the GET request method on the SearchR
 -- resource pattern. All of your resource patterns are defined in
@@ -62,17 +77,19 @@ postSearchR _ = do
     let submission = case result of
           FormSuccess res -> res
           _ -> ""
-        fieldQuery = text2Query submission
-        query' = querySplit fieldQuery
-        queryFilter = queryKeys2Filter $ fst <$> query'
+        fieldQuery = parse line "" submission
+        queryDBFilter = case fieldQuery of
+          Right parsed -> query2DBFilter parsed
+          Left _ -> []
+        queryFilter = case fieldQuery of
+          Right parsed -> query2Filter parsed
+          Left _ -> \_ -> False
 
-    possibleMatches <- runDB $ selectList queryFilter []
+    possibleMatches <- runDB $ selectList queryDBFilter []
 
-    let matches = filter (queryFilterRef query') (possibleMatches)
+    let matches = filter queryFilter possibleMatches
         handlerName = "postSearchR" :: Text
         records = not . null $ matches
-
-    liftIO $ print (fieldQuery, query', possibleMatches, matches)
 
     defaultLayout $ do
         aDomId <- newIdent
